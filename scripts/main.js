b487e4e5637f14dbd9fcda57b7c63ebc2473292e
Zhulien Zhivkov <julien.nedelinov7@gmail.com>
2026-03-08 02:18:18 +0200

    Add a bit of refactoring logic

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
+import helpers from './helpers.js';
+
 const initialState = {
   // input field values
   bill: 0,
-  numberOfPpl: 0,
+  numberOfPpl: 1,
   percentage: 0,
   // total values (tip and total)
   tip: 0,
@@ -14,18 +16,7 @@ const initialState = {
 };
 
 // shallow copy since it's a simple one-level object
-const state = { ...initialState };
-
-let currentlyActiveBtn = null;
-
-let bill = 0;
-let numberOfPpl = 0;
-let percentage = 0;
-
-let tip = 0;
-let total = 0;
-let tipPP = tip / numberOfPpl || tip;
-let totalPP = total / numberOfPpl || total;
+let state = { ...initialState };
 
 const billInput = document.querySelector('input[id=bill]');
 const numberOfPplInput = document.querySelector('input[id=number-ppl]');
@@ -42,215 +33,168 @@ const customPercentageContainer = document.querySelector(
 
 const resetButton = document.querySelector('.result .reset-btn');
 
-resetButton.addEventListener('click', () => {
+const inputs = document.querySelectorAll('input');
+
+const handleResetBtnClick = () => {
   // all html elements' values/textContents to 0/''
-  billInput.value = '';
-  numberOfPplInput.value = '';
-  customPercentageInput.value = '';
-  tipSum.textContent = '0.00';
-  totalSum.textContent = '0.00';
+  helpers.resetInputValues(document.querySelectorAll('input'));
+  helpers.resetNumericFields(document.querySelectorAll('span.numeric'));
+  // hide the custom percentage input's container
+  helpers.hideContainer(document.querySelector('.custom-percentage-container'));
 
-  // button deactivation
-  customPercentageContainer.classList.add('hidden');
-  currentlyActiveBtn.classList.remove('active');
-  currentlyActiveBtn = null;
+  // reset the state to the initial state
+  state = { ...initialState };
 
-  bill = 0;
-  numberOfPpl = 0;
-  percentage = 0;
+  // deactivate the button
+  helpers.deactivateButton(state);
 
-  tip = 0;
-  total = 0;
-  tipPP = 0;
-  totalPP = 0;
+  // disables the 'reset' button
+  helpers.disableBtn(resetButton);
 
-  disableResetBtn();
+  // recalculate the
+  handleRecalc(state);
+};
+
+resetButton.addEventListener('click', () => {
+  handleResetBtnClick();
 });
+
+const handleDeactivateCustomBtn = () => {
+  helpers.hideContainer(customPercentageContainer);
+  handleRecalc({ ...state, percentage: 0 });
+};
+
+const handleSameBtnDeactivation = (targetBtn, state) => {
+  targetBtn.classList.remove('active');
+  state.currentlyActiveBtn = null;
+  helpers.disableBtn(resetButton);
+};
+
+const handleRecalc = (state) => {
+  const { bill, numberOfPpl, percentage } = state;
+  const tip1 = helpers.calculateTip(bill, percentage, numberOfPpl);
+  const total1 = helpers.calculateTotal(bill, tip1, numberOfPpl);
+
+  helpers.populateNumericField(tipSum, tip1);
+  helpers.populateNumericField(totalSum, total1);
+};
 
 document.querySelectorAll('.percentage-btn').forEach((btn) =>
   btn.addEventListener('click', (e) => {
-    if (!btn.classList.contains('custom')) {
-      customPercentageContainer.classList.add('hidden');
-    } else {
-      customPercentageContainer.classList.toggle('hidden');
-      customPercentageInput.value = 0;
-    }
-
-    total -= tip;
-
-    if (currentlyActiveBtn) {
-      if (e.target.textContent === currentlyActiveBtn.textContent) {
-        percentage = 0;
-        tip = 0;
-
-        tipPP = tip / numberOfPpl;
-        totalPP = total / numberOfPpl;
-
-        tipSum.textContent =
-          numberOfPpl > 0 ? tipPP.toFixed(2) : tip.toFixed(2);
-        totalSum.textContent =
-          numberOfPpl > 0 ? totalPP.toFixed(2) : total.toFixed(2);
-
-        e.target.classList.remove('active');
-        currentlyActiveBtn = null;
-        disableResetBtn();
-        return;
-      }
-
-      currentlyActiveBtn.classList.remove('active');
-    }
-
-    enableResetBtn();
-
-    e.target.classList.add('active');
-    currentlyActiveBtn = e.target;
-
-    if (!btn.classList.contains('custom')) {
-      percentage = Number(e.target.textContent.replace('%', ''));
-      if (percentage > 0) {
-        tip = (total * percentage) / 100;
-      }
-    } else {
-      percentage = 0;
-      tip = 0;
-    }
-
-    total += tip;
-
-    tipPP = tip / numberOfPpl;
-    totalPP = total / numberOfPpl;
-
-    tipSum.textContent = numberOfPpl > 0 ? tipPP.toFixed(2) : tip.toFixed(2);
-    totalSum.textContent =
-      numberOfPpl > 0 ? totalPP.toFixed(2) : total.toFixed(2);
-  }),
-);
-
-// const handleCustomBtnClick = (btn, container) => {
-//   if (!btn.classList.contains('custom')) {
-//     container.classList.add('hidden');
-//   } else {
-//     container.classList.toggle('hidden');
-//   }
-// };
-
-document.querySelectorAll('input').forEach((input) => {
-  input.addEventListener('keypress', (e) => {
-    const regexForPeople = /[0-9]/;
-    const regexForSums = /[0-9.]/;
+    const { currentlyActiveBtn } = state;
+    const btn = e.target;
 
     if (
-      e.target.classList.contains('floating-value') &&
-      !regexForSums.test(e.key)
+      currentlyActiveBtn &&
+      currentlyActiveBtn.classList.contains('custom-btn')
     ) {
-      e.preventDefault();
+      handleDeactivateCustomBtn();
+    }
+
+    if (
+      currentlyActiveBtn &&
+      btn.textContent === currentlyActiveBtn.textContent
+    ) {
+      state.percentage = 0;
+
+      handleSameBtnDeactivation(btn, state);
+      handleRecalc(state);
       return;
     }
 
-    if (
-      e.target.classList.contains('absolute-value') &&
-      !regexForPeople.test(e.key)
-    ) {
-      e.preventDefault();
-    }
-  });
+    state.percentage = Number(btn.textContent.replace('%', ''));
 
+    enableResetBtn();
+
+    currentlyActiveBtn && currentlyActiveBtn.classList.remove('active');
+    btn.classList.add('active');
+    state.currentlyActiveBtn = btn;
+
+    handleRecalc(state);
+  }),
+);
+
+document.querySelector('.custom-btn').addEventListener('click', (e) => {
+  const btn = e.target;
+  if (btn.classList.contains('active')) {
+    // deactivate it and hide the container
+    helpers.deactivateButton(state);
+    helpers.hideContainer(customPercentageContainer);
+  } else {
+    // activate it and show the container
+    helpers.activateButton(btn, state);
+    helpers.showContainer(customPercentageContainer);
+    handleRecalc({ ...state, percentage: 0 });
+  }
+});
+
+const handleInputKeypress = (e) => {
+  const input = e.target;
+  const regexForAbsoluteNumValues = /[0-9]/;
+  const regexForFloatingNumValues = /[0-9.]/;
+
+  if (
+    input.classList.contains('floating-value') &&
+    !regexForFloatingNumValues.test(e.key)
+  ) {
+    e.preventDefault();
+    return;
+  }
+
+  if (
+    input.classList.contains('absolute-value') &&
+    !regexForAbsoluteNumValues.test(e.key)
+  ) {
+    e.preventDefault();
+  }
+};
+
+inputs.forEach((input) => {
+  input.addEventListener('keypress', handleInputKeypress);
   input.addEventListener('change', (e) => {
-    if (
-      +e.target.value === 0 &&
-      e.target.classList.contains('absolute-value')
-    ) {
+    const input = e.target;
+    if (+input.value === 0 && input.classList.contains('absolute-value')) {
       e.preventDefault();
-      e.target.value = 1;
+      input.value = 1;
     }
 
-    e.target.value = +e.target.value;
-    const value = +e.target.value;
-    const inputId = e.target.id;
-
-    // let tip = 0;
-    // let total = 0;
-    // let tipPP = tip / numberOfPpl || tip;
-    // let totalPP = total / numberOfPpl || total;
+    input.value = +input.value;
+    const value = +input.value;
+    const inputId = input.id;
 
     switch (inputId) {
       case 'bill':
         {
-          if (value === 0) {
-            tip = 0;
-            total = 0;
-            tipPP = 0;
-            totalPP = 0;
-            tipSum.textContent = '0.00';
-            totalSum.textContent = '0.00';
+          state.bill = value;
 
-            disableResetBtn();
+          if (value === 0) {
+            handleRecalc(state);
+            helpers.disableBtn(resetButton);
             return;
           }
 
           if (resetButton.disabled) {
             enableResetBtn();
           }
-
-          total -= tip;
-
-          if (percentage > 0) {
-            tip = value * (percentage / 100);
-          }
-
-          total = value + tip;
-
-          tipPP = tip / numberOfPpl;
-          totalPP = total / numberOfPpl;
-
-          tipSum.textContent =
-            numberOfPpl > 0 ? tipPP.toFixed(2) : tip.toFixed(2);
-          totalSum.textContent =
-            numberOfPpl > 0 ? totalPP.toFixed(2) : total.toFixed(2);
         }
         break;
       case 'number-ppl':
         {
-          numberOfPpl = value;
-          tipPP = tip / numberOfPpl;
-          totalPP = total / numberOfPpl;
-
-          tipSum.textContent =
-            numberOfPpl > 0 ? tipPP.toFixed(2) : tip.toFixed(2);
-          totalSum.textContent =
-            numberOfPpl > 0 ? totalPP.toFixed(2) : total.toFixed(2);
+          state.numberOfPpl = value;
         }
         break;
       case 'custom-percentage':
         {
-          total -= tip;
-          percentage = value;
-          tip = total * (percentage / 100);
-          total += tip;
-
-          tipPP = tip / numberOfPpl;
-          totalPP = total / numberOfPpl;
-
-          tipSum.textContent =
-            numberOfPpl > 0 ? tipPP.toFixed(2) : tip.toFixed(2);
-          totalSum.textContent =
-            numberOfPpl > 0 ? totalPP.toFixed(2) : total.toFixed(2);
+          state.percentage = value;
         }
         break;
     }
+
+    handleRecalc(state);
   });
 });
-
-function updateTip(sum) {}
-
-function updateTotal(sum) {}
-
 function enableResetBtn() {
   document.querySelector('.reset-btn').disabled = false;
-}
-
-function disableResetBtn() {
-  document.querySelector('.reset-btn').disabled = true;
 }
 
 // !! The only visible limitation in my calculator is when the bill is at least 100 times less than the number of people (edge case)
